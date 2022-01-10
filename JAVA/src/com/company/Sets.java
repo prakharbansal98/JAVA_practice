e44c3e8f2fb94bec28c265ca135ec9e0ecf15f98
Prakhar Bansal <prakhar.b98@gmail.com>
2022-01-10 21:17:35 +0530

    Set

---
+++ JAVA/src/com/company/Sets.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new TreeSet<>();
+
+        set1.add(1);
+        set1.add(1);
+        System.out.println(set1);
+
+        set1.add(2);
+        set1.add(4);
+        set1.add(5);
+        set1.add(3);
+        System.out.println(set1);
+
+        for(Integer i:set1){
+            System.out.println(i);
+        }
+
+        Set<Integer> set2 = new HashSet<>();
+
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+
+
+
+
+
+
+    }
+}
